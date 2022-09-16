ba583e0b602b102428bad51876009958d9e41901
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-16 23:52:05 +0500

    Topshiriq role

---
+++ app/src/main/java/app/app/topshiriq/AllUsers.java
@@ -0,0 +1,13 @@
+package app.app.topshiriq;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_users);
+    }
+}
